a7c8eb51fa2ee9d72e95e89ad2724503959cc749
unknown <chima.okwuokei@pau.edu.ng>
2025-05-09 15:39:26 +0100

    feat: completed hand physics

--- vr-volleyball-training/Assets/HandPresencePhysicsScript.cs
+++ vr-volleyball-training/Assets/HandPresencePhysicsScript.cs
@@ -6,11 +6,42 @@ public class HandPresencePhysicsScript : MonoBehaviour
 {
     public Transform target;
     private Rigidbody rb;
+    public Renderer nonPhysicalHand;
+    public float showNonPhysicalHandDistance = 0.05f;
+
+    private Collider[] handColliders;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        handColliders = GetComponentsInChildren<Collider>();
     }
+
+    public void EnableHandCollider(){
+        foreach(var item in handColliders){
+            item.enabled = true;
+        }
+    }
+    public void EnableHandColliderDelay(float delay){
+        Invoke("EnableHandCollider", delay);
+    }
+    public void DisableHandCollider(){
+        foreach(var item in handColliders){
+            item.enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(transform.position, target.position);
+
+        if(distance>showNonPhysicalHandDistance){
+            nonPhysicalHand.enabled =true;
+        }else{
+            nonPhysicalHand.enabled =false;
+        }
+    }
+
 
     // Update is called once per frame
     void FixedUpdate()
